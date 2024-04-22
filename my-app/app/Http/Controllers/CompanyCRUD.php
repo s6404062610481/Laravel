47e02d8f19d43d73103b8e3fb434b65918368147
BigcBow <s6404062610481@email.kmutnb.ac.th>
2024-04-22 10:47:04 +0700

    update

--- my-app/app/Http/Controllers/CompanyCRUD.php
+++ my-app/app/Http/Controllers/CompanyCRUD.php
@@ -2,9 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\Model\Company;
 use Illuminate\Http\Request;
 
 class CompanyCRUD extends Controller
 {
     //
+    //create index
+
+    public function index(){
+        $data['companies'] = Company::orderBy('id', 'desc')->paginate(5);
+        return view('companies.index', $data);
+    }
+
+    public function create(){
+        return view('companies.create');
+    }
 }
