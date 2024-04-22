0fc3c3b6ca7807a4a5ebebcbb852bfcc66ae208d
BigcBow <s6404062610481@email.kmutnb.ac.th>
2024-04-22 11:18:39 +0700

    Update CompanyCRUD.php

--- my-app/app/Http/Controllers/CompanyCRUD.php
+++ my-app/app/Http/Controllers/CompanyCRUD.php
@@ -3,8 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Company;
 
 class CompanyCRUD extends Controller
 {
     //
+    public function index(){
+        $data['companies'] = Company::orderBy('id', 'desc')->paginate(5);
+        return view('companies.index', $data);
+    }
+
+    public function create(){
+        return view('companies.create');
+    }
 }
