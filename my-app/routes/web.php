a3241176fa34f9bc8f1c5f59927deef4a893c06c
BigcBow <s6404062610481@email.kmutnb.ac.th>
2024-04-22 11:24:53 +0700

    create index page

--- my-app/routes/web.php
+++ my-app/routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CompanyCRUD;
+
+Route::resource('companies', CompanyCRUD::class);
 
 /*
 |--------------------------------------------------------------------------
