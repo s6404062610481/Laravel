5b4fb73fd70da6497fefc68e72f649ab30103a2c
BigcBow <s6404062610481@email.kmutnb.ac.th>
2024-04-22 14:15:28 +0700

    Create Create Page

--- my-app/resources/views/companies/index.blade.php
+++ my-app/resources/views/companies/index.blade.php
@@ -5,8 +5,41 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>PHP Laravel CRUD</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 </head>
 <body>
-    <h1>Helloooooo</h1>
+    <div class="container mt-2">
+        <div class="row">
+            <div class="col-lg-12 text-center">
+                <h2>PHP Laravel CRUD</h2>
+            </div>
+            <div>
+                <a href="{{ route('companies.create') }}" class="btn btn-success">Create Company</a>
+            </div>
+            @if($message = Session::get('success'))
+            <div class="alert alert-success">
+                <p>{{ $message }}</p>
+            </div>
+            @endif
+
+            <table class="table table-bordered">
+                <tr>
+                    <th>No.</th>
+                    <th>Company Name</th>
+                    <th>Company Email</th>
+                    <th>Company Address</th>
+                    <th width="280px">Action</th>
+                </tr>
+                @foreach ($companies as $company)
+                    <tr>
+                        <td>{{ $company->id }}</td>
+                        <td>{{ $company->name }}</td>
+                        <td>{{ $company->email }}</td>
+                        <td>{{ $company->address }}</td>
+                    </tr>
+                @endforeach
+            </table>
+        </div>
+    </div>
 </body>
 </html>
